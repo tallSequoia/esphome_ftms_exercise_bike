0b277ba4d74bbf47131690d21e03074a6c8e007a
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2023-12-23 08:19:37 +0000

    Update __init__.py

--- components/ftms_exercise_bike/__init__.py
+++ components/ftms_exercise_bike/__init__.py
@@ -2,10 +2,12 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import core
 
-ftms_exercise_bike_ns = cg.esphome_ns.namespace('ftms_exercise_bike')
-FTMSExerciseBike = ftms_exercise_bike_ns.class_('FTMSExerciseBike', cg.PollingComponent, 
-    filename="ftms_exercise_bike.h")
+# Define the namespace based on C++ namespace
+tallsequoia_ns = cg.esphome_ns.namespace('tallsequoia')
 
+# Define the class within the created namespace
+FTMSExerciseBike = tallsequoia_ns.class_('FTMSExerciseBike', cg.PollingComponent, 
+    filename="ftms_exercise_bike.h")
 CONFIG_SCHEMA = cv.Schema({})
 
 def to_code(config):
