affc05ebe104cb3dbf0405c73168292066b83f0c
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2023-12-23 08:25:37 +0000

    Update __init__.py

--- components/ftms_exercise_bike/__init__.py
+++ components/ftms_exercise_bike/__init__.py
@@ -6,10 +6,11 @@ from esphome import core
 tallsequoia_ns = cg.esphome_ns.namespace('tallsequoia')
 
 # Define the class within the created namespace
-FTMSExerciseBike = tallsequoia_ns.class_('FTMSExerciseBike', cg.PollingComponent, 
-    filename="ftms_exercise_bike.h")
+FTMSExerciseBike = tallsequoia_ns.class_('FTMSExerciseBike', cg.PollingComponent)
+
 CONFIG_SCHEMA = cv.Schema({})
 
 def to_code(config):
+    # Create an instance of the FTMSExerciseBike class
     var = cg.new_Pvariable(FTMSExerciseBike)
     cg.add(var.set_update_interval(1000))  # Update interval in milliseconds
