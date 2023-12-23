d8f06b941918b81e3c1aa137515e611ca906393d
Tall Sequoia <24196105+tallSequoia@users.noreply.github.com>
2023-12-23 08:07:51 +0000

    Update ftms_exercise_bike.h

--- components/ftms_exercise_bike/ftms_exercise_bike.h
+++ components/ftms_exercise_bike/ftms_exercise_bike.h
@@ -1,13 +1,22 @@
+#pragma once
+
 #ifndef FTMS_EXERCISE_BIKE_H
 #define FTMS_EXERCISE_BIKE_H
 
+#include "esphome.h"
+#include "esphome/core/component.h"
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-#include "esphome.h"
+
+
+namespace esphome {
+namespace tallsequoia {
 
 class FTMSExerciseBike : public PollingComponent {
 public:
+    FTMSExerciseBike() {}
+
     void setup() override;
     void update() override;
 
@@ -17,5 +26,8 @@ private:
     uint16_t calculateWheelRevolution();
     float generateRandomFloat(float min, float max);
 };
+
+} // namespace tallsequoia
+} // namespace esphome
 
 #endif
